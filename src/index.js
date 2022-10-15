11f40808d901cc03a2d8777ff7f2f8332aa7231e
eriksensiri <siri.husby@gmail.com>
2022-10-15 22:20:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function cityTemperature(response) {
   currentCity.innerHTML = `${city}`;
 
   let nowWeather = document.querySelector("#now-weather");
-  nowWeather.innerHTML = response.data.weather[0].toUpperCase.description;
+  nowWeather.innerHTML = response.data.weather[0].description;
   let lowestTemperature = Math.round(response.data.main.temp_min);
   let minTemperature = document.querySelector(".min-temperature");
   minTemperature.innerHTML = `${lowestTemperature}`;
