530edbf4c33d22da430eefd15feaf935695bf6b5
eriksensiri <siri.husby@gmail.com>
2022-10-15 21:42:14 +0200

    Corrected weather description

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function cityTemperature(response) {
   currentCity.innerHTML = `${city}`;
 
   let nowWeather = document.querySelector("#now-wheather");
-  nowWeather.innerHTML = response.data.main[0].description;
+  nowWeather.innerHTML = response.data.main.description;
   let lowestTemperature = Math.round(response.data.main.temp_min);
   let minTemperature = document.querySelector(".min-temperature");
   minTemperature.innerHTML = `${lowestTemperature}`;
