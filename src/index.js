a305873867c20035fb2f288d51f87ff28440ce6d
eriksensiri <siri.husby@gmail.com>
2022-10-16 13:49:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ let fahrenheitTemperature = document.querySelector("#fahrenheit");
 fahrenheitTemperature.addEventListener("click", fahrenheitConverter);
 
 function showIcon() {
-  if (now - weather === "clear sky") return `icons/sun.png`;
+  if (nowWeather === "clear sky") return `icons/sun.png`;
 }
 
 function cityTemperature(response) {
