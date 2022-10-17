40a2c88b901e0b6898f1512a11b22eb09962c8e7
eriksensiri <siri.husby@gmail.com>
2022-10-17 16:07:56 +0200

    Updating icons weather

--- src/index.js
+++ src/index.js
@@ -50,11 +50,14 @@ function cityTemperature(response) {
   let currentCity = document.querySelector("#current-location");
   currentCity.innerHTML = `${city}`;
 
-  nowWeather = response.data.weather[0].description;
+  nowWeather = response.data.weather[0].icon;
   let actualIcon = weatherEmoji();
   let iconElement = document.querySelector("#weather-emoji");
-  iconElement.setAttribute("src", actualIcon);
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].icon);
 
   let lowestTemperature = Math.round(response.data.main.temp_min);
   let minTemperature = document.querySelector(".min-temperature");
