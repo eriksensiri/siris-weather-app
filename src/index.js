76f1bb6280873d01d28ff49216f2f47c5f123316
eriksensiri <siri.husby@gmail.com>
2022-10-17 16:12:26 +0200

    Removing own description

--- src/index.js
+++ src/index.js
@@ -74,67 +74,6 @@ function cityTemperature(response) {
 }
 
 function weatherEmoji() {
-  if (nowWeather === "clear sky") {
-    return `src/icons/sun.png`;
-  }
-  if (nowWeather === "clear") {
-    return `src/icons/sun.png`;
-  }
-  if (nowWeather === "clouds") {
-    return `src/icons/cloud_sun.png`;
-  }
-  if (nowWeather === "few clouds") {
-    return `src/icons/cloud_sun.png`;
-  }
-
-  if (nowWeather === "scattered clouds") {
-    return `src/icons/cloudy_sun.png`;
-  }
-  if (nowWeather === "dust") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "smoke") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "sand") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "ash") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "squall") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "broken clouds") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "fog") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "haze") {
-    return `src/icons/clouds.png`;
-  }
-  if (nowWeather === "shower rain") {
-    return `src/icons/sun.png`;
-  }
-  if (nowWeather === "drizzel") {
-    return `src/icons/ligth_rain.png`;
-  }
-  if (nowWeather === "rain") {
-    return `src/icons/ligth_rain.png`;
-  }
-  if (nowWeather === "thunderstorm") {
-    return `src/icons/cloud_rain_ligthing.png`;
-  }
-  if (nowWeather === "tornado") {
-    return `src/icons/cloud_rain_ligthing.png`;
-  }
-  if (nowWeather === "snow") {
-    return `src/icons/cloud_snow_rain.png`;
-  }
-  if (nowWeather === "mist") {
-    return `src/icons/cloud_wind.png`;
-  }
 }
 
 function searchfunction(city) {
